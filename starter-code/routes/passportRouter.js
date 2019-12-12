4af55d8ff32cd968593c19e2606dfbda1277807c
marinaalvarez <55624848+marinalvarez7@users.noreply.github.com>
2019-12-12 23:59:04 +0100

    done

--- starter-code/routes/passportRouter.js
+++ starter-code/routes/passportRouter.js
@@ -2,15 +2,77 @@ const express        = require("express");
 const passportRouter = express.Router();
 // Require user model
 
-// Add bcrypt to encrypt passwords
+const User = require("../models/user");
+
+// Bcrypt to encrypt passwords
+const bcrypt = require("bcryptjs");
+const bcryptSalt = 10;
 
 // Add passport 
 
+const passport = require("passport");
 
 const ensureLogin = require("connect-ensure-login");
 
+passportRouter.get("/signup", (req, res, next) => {
+  res.render("passport/signup");
+});
+
+passportRouter.post("/signup", (req, res, next) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  // 1. Check username and password are not empty
+  if (username === "" || password === "") {
+    res.render("passport/signup", { errorMessage: "Indicate username and password" });
+    return;
+  }
+
+  User.findOne({ username })
+    .then(user => {
+      // 2. Check user does not already exist
+      if (user) {
+        res.render("passport/signup", { errorMessage: "The username already exists" });
+        return;
+      }
+
+      // Encrypt the password
+      const salt = bcrypt.genSaltSync(bcryptSalt);
+      const hashPass = bcrypt.hashSync(password, salt);
+
+      //
+      // Save the user in DB
+      //
+
+      const newUser = new User({
+        username,
+        password: hashPass
+      });
+
+      newUser.save()
+        .then(user => res.redirect("/"))
+        .catch(err => next(err))
+      ;
+        
+    })
+    .catch(err => next(err))
+  ;
+});
+
+passportRouter.get("/login", (req, res, next) => {
+  res.render("passport/login");
+});
+
+passportRouter.post("/login", passport.authenticate("local", {
+  successRedirect: "/",
+  failureRedirect: "/login"
+}));
 
 passportRouter.get("/private-page", ensureLogin.ensureLoggedIn(), (req, res) => {
+  if (!req.user) {
+    res.redirect('/login'); // not logged-in
+    return;
+  }
   res.render("passport/private", { user: req.user });
 });
 
