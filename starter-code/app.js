41ea21d0c17488554f24576b109b21b43bbc29ee
Tracy Bonjour <tracybonjour@macbook-air-de-tracy.home>
2019-12-12 21:56:08 +0100

    done

--- starter-code/app.js
+++ starter-code/app.js
@@ -9,6 +9,14 @@ const mongoose     = require('mongoose');
 const logger       = require('morgan');
 const path         = require('path');
 
+const session    = require("express-session");
+const MongoStore = require('connect-mongo')(session);
+
+const bcrypt = require("bcrypt");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+
+const User = require('./models/user.js')
 
 mongoose
   .connect('mongodb://localhost/starter-code', {useNewUrlParser: true})
@@ -44,7 +52,49 @@ app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
+app.use(session({
+  secret: "our-passport-local-strategy-app",
+  store: new MongoStore( { mongooseConnection: mongoose.connection }),
+  resave: true,
+  saveUninitialized: true,
+}));
 
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser((user, cb) => {
+  cb(null, user._id);
+});
+passport.deserializeUser((id, cb) => {
+  User.findById(id)
+    .then(user => cb(null, user))
+    .catch(err => cb(err))
+  ;
+});
+
+passport.use(new LocalStrategy(
+  {passReqToCallback: true},
+  (...args) => {
+    const [req,,, done] = args;
+
+    const {username, password} = req.body;
+
+    User.findOne({username})
+      .then(user => {
+        if (!user) {
+          return done(null, false, { message: "Incorrect username" });
+        }
+          
+        if (!bcrypt.compareSync(password, user.password)) {
+          return done(null, false, { message: "Incorrect password" });
+        }
+    
+        done(null, user);
+      })
+      .catch(err => done(err))
+    ;
+  }
+));
 
 // default value for title local
 app.locals.title = 'Express - Generated with IronGenerator';
--- starter-code/routes/passportRouter.js
+++ starter-code/routes/passportRouter.js
@@ -1,16 +1,81 @@
 const express        = require("express");
 const passportRouter = express.Router();
 // Require user model
+const User = require("../models/user");
+
+const passport = require("passport");
 
 // Add bcrypt to encrypt passwords
+const bcrypt = require("bcrypt");
+const bcryptSalt = 10;
 
 // Add passport 
+passportRouter.get("/signup", (req, res, next) => {
+  res.render("passport/signup");
+});
 
+passportRouter.post("/signup", (req, res, next) => {
+  const username = req.body.username;
+  const password = req.body.password;
 
-const ensureLogin = require("connect-ensure-login");
+  // 1. Check username and password are not empty
+  if (username === "" || password === "") {
+    res.render("passport/signup", { errorMessage: "Indicate username and password" });
+    return;
+  }
 
+  User.findOne({ username })
+    .then(user => {
+      // 2. Check user does not already exist
+      if (user) {
+        res.render("passport/signup", { errorMessage: "The username already exists" });
+        return;
+      }
+      // Encrypt the password
+      const salt = bcrypt.genSaltSync(bcryptSalt);
+      const hashPass = bcrypt.hashSync(password, salt);
 
-passportRouter.get("/private-page", ensureLogin.ensureLoggedIn(), (req, res) => {
+      //
+      // Save the user in DB
+      //
+
+      const newUser = new User({
+        username,
+        password: hashPass
+      });
+
+      newUser.save()
+        .then(user => res.redirect("/"))
+        .catch(err => next(err))
+      ;
+        
+    })
+    .catch(err => next(err))
+  ;
+});
+
+passportRouter.get("/login", (req, res, next) => {
+  res.render("passport/login");
+});
+
+passportRouter.post("/login", passport.authenticate("local", {
+  successRedirect: "/",
+  failureRedirect: "/login"
+}));
+
+//const ensureLogin = require("connect-ensure-login");
+
+//passportRouter.get("/private-page", ensureLogin.ensureLoggedIn(), (req, res) => {
+//  res.render("passport/private", { user: req.user });
+//});
+
+passportRouter.get("/private-page", (req, res) => {
+  if (!req.user) {
+    res.redirect('/login'); // not logged-in
+    return;
+  }
+  
+  // ok, req.user is defined
   res.render("passport/private", { user: req.user });
 });
 
